0419ca8aeb2ac21ddf19c03ab748f33e8c0df660
Jeffrey K Gillan <jgillan@email.arizona.edu>
2023-04-14 12:43:27 -0700

    Update pdal_copc.sh

--- pdal_copc.sh
+++ pdal_copc.sh
@@ -4,7 +4,7 @@
 pipeline="/app/copc.json"
 #pipeline="./copc.json"
 
-# Loop over LAS/LAZ files in the current directory and subdirectories
+# Loop over LAS/LAZ files in a directory and subdirectories. It this example, it loops over `/data` in the container. 
 find /data -type f \( -name "*.las" -o -name "*.laz" \) -print0 | while IFS= read -r -d '' file; do
     # Get the file extension
     extension="${file##*.}"

6e403f37bad7a3c4123e866e3595e259781146c5
Jeffrey K Gillan <jgillan@email.arizona.edu>
2023-11-15 13:39:22 -0700

    Update pdal_csf.sh

--- pdal_csf.sh
+++ pdal_csf.sh
@@ -1,18 +1,17 @@
 #!/bin/bash
 
 # Define the pipeline JSON file
-pipeline="/app/copc.json"
-#pipeline="./copc.json"
+pipeline="/app/filter_csf.json"
 
 # Loop over LAS/LAZ files in a directory and subdirectories. It this example, it loops over `/data` in the container. 
-find /data -type f \( -name "*.las" -o -name "*.copc.laz" \) -print0 | while IFS= read -r -d '' file; do
+find /data -type f \( -name "*.laz" -o -name "*.copc.laz" \) -print0 | while IFS= read -r -d '' file; do
     # Get the file extension
     extension="${file##*.}"
 
     # Run the pipeline with the appropriate reader based on the file extension
-    if [[ "$extension" == "las" ]]; then
+    if [[ "$extension" == "laz" ]]; then
         pdal pipeline -i "$pipeline" --readers.las.filename="$file" --writers.copc.filename="${file%.las}.copc.laz"
-    elif [[ "$extension" == "laz" ]]; then
+    elif [[ "$extension" == "copc.laz" ]]; then
         pdal pipeline -i "$pipeline" --readers.las.filename="$file" --writers.copc.filename="${file%.laz}.copc.laz"
     fi
 done

fc7cded68c793dbc2eb626598f8537cdf2538e13
Jeffrey K Gillan <jgillan@email.arizona.edu>
2023-11-15 14:20:57 -0700

    Update pdal_csf.sh

--- pdal_csf.sh
+++ pdal_csf.sh
@@ -10,8 +10,8 @@ find /data -type f \( -name "*.laz" -o -name "*.copc.laz" \) -print0 | while IFS
 
     # Run the pipeline with the appropriate reader based on the file extension
     if [[ "$extension" == "laz" ]]; then
-        pdal pipeline -i "$pipeline" --readers.las.filename="$file" --writers.copc.filename="filtered_${file}"
+        pdal pipeline -i "$pipeline" --readers.las.filename="$file" --writers.copc.filename="filtered_${file%.laz}.copc.laz"
     elif [[ "$extension" == "copc.laz" ]]; then
-        pdal pipeline -i "$pipeline" --readers.las.filename="$file" --writers.copc.filename="filtered_${file}"
+        pdal pipeline -i "$pipeline" --readers.las.filename="$file" --writers.copc.filename="filtered_${file%.copc.laz}.copc.laz"
     fi
 done

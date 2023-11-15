6fa6e41209e553948d785286f7ccd24ec88d53b8
Jeffrey K Gillan <jgillan@email.arizona.edu>
2023-11-15 13:37:06 -0700

    Update and rename pdal_copc.sh to pdal_csf.sh

--- pdal_csf.sh
+++ pdal_csf.sh
@@ -5,7 +5,7 @@ pipeline="/app/copc.json"
 #pipeline="./copc.json"
 
 # Loop over LAS/LAZ files in a directory and subdirectories. It this example, it loops over `/data` in the container. 
-find /data -type f \( -name "*.las" -o -name "*.laz" \) -print0 | while IFS= read -r -d '' file; do
+find /data -type f \( -name "*.las" -o -name "*.copc.laz" \) -print0 | while IFS= read -r -d '' file; do
     # Get the file extension
     extension="${file##*.}"
 
